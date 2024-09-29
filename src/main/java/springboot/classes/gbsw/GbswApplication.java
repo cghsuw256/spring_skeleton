13a25b3180020bc2c6e626c6a239bdb0462bc72b
cghsuw256 <c66182527@gmail.com>
2024-09-29 22:06:23 +0900

    initial commit

---
+++ src/main/java/springboot/classes/gbsw/GbswApplication.java
@@ -0,0 +1,13 @@
+package springboot.classes.gbsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbswApplication.class, args);
+	}
+
+}
